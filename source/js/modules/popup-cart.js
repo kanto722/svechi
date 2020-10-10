8506fc1c93c950cf5cbef4e075680a0bde3df776
Kanto Kanto72 <kanto72@yandex.ru>
2020-10-10 19:02:52 +0300

    two

--- source/js/modules/popup-cart.js
+++ source/js/modules/popup-cart.js
@@ -7,13 +7,3 @@ function PopUpShow(){
 function PopUpHide(){
     $("#popup1").hide();
 }
-
-// $(".popup-cart").click(function() {
-//     $('.toggled_block').toggle();
-//   });
-//   $(document).on('click', function(e) {
-//     if (!$(e.target).closest(".popup-cart__close").length) {
-//       $('.toggled_block').hide();
-//     }
-//     e.stopPropagation();
-//   });
